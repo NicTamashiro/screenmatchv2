39da8af944307fcecea1892c23df822d83f4b1cc
Nicolas <adilsonq1976@gmail.com>
2026-07-16 13:20:56 -0300

    Inicializando a aplicacao

--- src/main/java/br/com/nicolas/screenmatchv2/Screenmatchv2Application.java
+++ src/main/java/br/com/nicolas/screenmatchv2/Screenmatchv2Application.java
@@ -1,13 +1,20 @@
 package br.com.nicolas.screenmatchv2;
 
+import br.com.nicolas.screenmatchv2.principal.Main;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Screenmatchv2Application {
+public class Screenmatchv2Application implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Screenmatchv2Application.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Main main = new Main();
+		main.exibeMenu();
+	}
 }
