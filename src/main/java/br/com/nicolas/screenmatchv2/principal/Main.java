bc2504ce3bc6946d655d0ce8cd9781b5751b6547
Nicolas <adilsonq1976@gmail.com>
2026-07-16 13:30:58 -0300

    Classe Main

---
+++ src/main/java/br/com/nicolas/screenmatchv2/principal/Main.java
@@ -0,0 +1,85 @@
+package br.com.nicolas.screenmatchv2.principal;
+
+import br.com.nicolas.screenmatchv2.model.DadosSerie;
+import br.com.nicolas.screenmatchv2.model.DadosTemporada;
+import br.com.nicolas.screenmatchv2.service.ConsumoApi;
+import br.com.nicolas.screenmatchv2.service.ConverteDados;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+    private Scanner scanner = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+    private ConverteDados converte = new ConverteDados();
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=6585022c";
+    private List<DadosSerie> dadosSeries = new ArrayList<>();
+
+    public void exibeMenu(){
+        var opcao = -1;
+        while (opcao != 0) {
+            var menu = """
+                    ===== Screen Match V2 =====
+                    
+                    1 - Buscar séries
+                    2 - Buscar episódios
+                    3 - Listar séries buscadas
+                    
+                    0 - Sair
+                    """;
+
+            System.out.println(menu);
+            opcao = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (opcao) {
+                case 1:
+                    buscarSerieWeb();
+                    break;
+                case 2:
+                    buscarEpisodioPorSerie();
+                    break;
+                case 3:
+                    listarSeriesBuscadas();
+                    break;
+                case 0:
+                    System.out.println("Saindo...");
+                    break;
+                default:
+                    System.out.println("Opção inválida");
+            }
+        }
+    }
+
+    private DadosSerie getDadosSerie() {
+        System.out.println("Digite o nome da série para busca");
+        var nomeSerie = scanner.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+        return converte.obterDados(json, DadosSerie.class);
+    }
+
+    private void buscarSerieWeb(){
+        DadosSerie dados = getDadosSerie();
+        dadosSeries.add(dados);
+        System.out.println(dados);
+    }
+
+    private void buscarEpisodioPorSerie(){
+        DadosSerie dadosSerie = getDadosSerie();
+        List<DadosTemporada> temporadas = new ArrayList<>();
+
+        for(int i = 1; i <= dadosSerie.totalTemporadas(); i++){
+            var json = consumo.obterDados(ENDERECO + dadosSerie.titulo().replace(" ", "+") + "&season=" + i + API_KEY);
+            DadosTemporada dadosTemporada = converte.obterDados(json, DadosTemporada.class);
+            temporadas.add(dadosTemporada);
+        }
+        temporadas.forEach(System.out::println);
+    }
+
+    private void listarSeriesBuscadas(){
+        dadosSeries.forEach(System.out::println);
+    }
+}
