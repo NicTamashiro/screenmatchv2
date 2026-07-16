72c1f1af02a33f81b2e809bd0978c0b8a4805c90
Nicolas <adilsonq1976@gmail.com>
2026-07-16 13:05:12 -0300

    Initial commit

---
+++ src/main/java/br/com/nicolas/screenmatchv2/Screenmatchv2Application.java
@@ -0,0 +1,13 @@
+package br.com.nicolas.screenmatchv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Screenmatchv2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Screenmatchv2Application.class, args);
+	}
+
+}
