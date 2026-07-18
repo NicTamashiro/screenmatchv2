0345e4f6460d61637c10a1c0eaa99342c46f3fce
Nicolas <adilsonq1976@gmail.com>
2026-07-18 17:56:30 -0300

    Criando interface SerieRepository

---
+++ src/main/java/br/com/nicolas/screenmatchv2/repository/SerieRepository.java
@@ -0,0 +1,8 @@
+package br.com.nicolas.screenmatchv2.repository;
+
+import br.com.nicolas.screenmatchv2.model.Serie;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SerieRepository extends JpaRepository<Serie, Long> {
+
+}
