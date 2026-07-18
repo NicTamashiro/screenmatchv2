a22cabf153f32fee055d6af3a807d55230d5873e
Nicolas <adilsonq1976@gmail.com>
2026-07-18 18:03:06 -0300

    Colocando Autowired

--- src/main/java/br/com/nicolas/screenmatchv2/Screenmatchv2Application.java
+++ src/main/java/br/com/nicolas/screenmatchv2/Screenmatchv2Application.java
@@ -1,6 +1,8 @@
 package br.com.nicolas.screenmatchv2;
 
 import br.com.nicolas.screenmatchv2.principal.Main;
+import br.com.nicolas.screenmatchv2.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Screenmatchv2Application implements CommandLineRunner {
 
+	@Autowired
+	private SerieRepository repositorio;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Screenmatchv2Application.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Main main = new Main();
+		Main main = new Main(repositorio);
 		main.exibeMenu();
 	}
 }
